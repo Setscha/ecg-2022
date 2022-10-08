96cb75b010a1904dcd94aa145aea26cd07993b1c
Setscha <tobias.seczer@gmail.com>
2022-10-08 10:10:13 +0200

    [TASK] Adjust render loop order

--- ECG_Solution/src/Main.cpp
+++ ECG_Solution/src/Main.cpp
@@ -81,8 +81,8 @@ int main(int argc, char **argv) {
         glClearColor(1, 1, 1, 1);
 
         while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwPollEvents();
             drawTeapot();
             glfwSwapBuffers(window);
             /* Gitlab CI automatic testing */
