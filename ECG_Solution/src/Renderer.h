d3e23553bfa7708b84078714db620c8547163dca
Setscha <tobias.seczer@gmail.com>
2022-11-01 20:21:09 +0100

    [TASK] Add cylinder to renderer and cleanup renderer class

--- ECG_Solution/src/Renderer.h
+++ ECG_Solution/src/Renderer.h
@@ -1,12 +1,20 @@
 #pragma  once
 
+#define _USE_MATH_DEFINES
 #include "Utils.h"
 #include "Shader.h"
+#include <cmath>
 
 class Renderer {
 private:
+    GLuint vao;
+    GLuint vbo;
+    GLuint ibo;
 public:
     void clear() const;
     void drawTeapot(const Shader& shader) const;
-    void drawCube(float width, float height, float depth) const;
+    void drawCube(float width, float height, float depth);
+    void drawCylinder(int segments, float height, float radius);
+    void drawBuffers(int vertexBufferSize, int indexBufferSize, GLfloat* positions, GLuint* indices);
+    void cleanupBuffers() const;
 };
