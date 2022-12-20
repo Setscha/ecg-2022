7f1fbc51c14ca60f665f698cfabb236c4f2dd2fb
Setscha <tobias.seczer@gmail.com>
2022-12-20 18:36:50 +0100

    [TASK] Redo cube to hardcoded values for better code

--- ECG_Solution/src/Vertex.h
+++ ECG_Solution/src/Vertex.h
@@ -5,18 +5,4 @@
 struct Vertex {
     glm::vec3 position;
     glm::vec3 normal;
-    inline bool operator==(const Vertex& other) const {
-        return position == other.position && normal == other.normal;
-    }
 };
-
-namespace std {
-    template<>
-    struct hash<Vertex> {
-        inline long operator()(const Vertex& x) const {
-            // https://stackoverflow.com/questions/5928725/hashing-2d-3d-and-nd-vectors
-            return ((long)(x.position.x * 1000) * 73856093 ^ (long)(x.position.y * 1000) * 83492791 ^ (long)(x.position.z * 1000) * 10619863) +
-                    ((long)(x.normal.x * 1000) * 73856093 ^ (long)(x.normal.y * 1000) * 10619863 ^ (long)(x.normal.z * 1000) * 83492791);
-        }
-    };
-}
